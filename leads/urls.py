8ff1d696b92fc908fbcc886f90beef10be6c5e79
harendrakumar16 <harendrakumar.h@netmeds.com>
2021-12-27 21:08:30 +0530

    first commit

---
+++ leads/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import lead_delete, lead_detail, lead_list,lead_create, lead_update
+
+app_name = 'leads'
+
+urlpatterns = [
+    path('', lead_list, name= 'lead-list'),
+    path("create/",lead_create, name = 'lead-create'),
+    path("<int:pk>/update/",lead_update, name = 'lead-update'),
+    path("<int:pk>/delete/",lead_delete, name = 'lead-delete'),   
+    path("<int:pk>/",lead_detail, name= 'lead-detail')
+]
